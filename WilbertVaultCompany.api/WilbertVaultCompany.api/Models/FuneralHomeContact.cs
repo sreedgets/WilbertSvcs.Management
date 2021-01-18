63fcaffda970467c6d6ddb28ab091459eedbd833
Kirk Thomas <dablumaroon@gmail.com>
2021-01-18 18:13:56 -0500

    checking in

--- WilbertVaultCompany.api/WilbertVaultCompany.api/Models/FuneralHomeContact.cs
+++ WilbertVaultCompany.api/WilbertVaultCompany.api/Models/FuneralHomeContact.cs
@@ -11,6 +11,7 @@ namespace WilbertVaultCompany.api.Models
         public int FuneralHomeContactId { get; set; }
         public int FuneralHomeId { get; set; }
 
+        public string fhName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string NickName { get; set; }
