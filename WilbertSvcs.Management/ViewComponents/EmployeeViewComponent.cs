52d3d4e7ec341313639aabc4550a6158b8353bf8
Kirk Thomas <dablumaroon@gmail.com>
2021-02-09 15:30:30 -0500

    Employee, plant, and truck are now working.

---
+++ WilbertSvcs.Management/ViewComponents/EmployeeViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WilbertVaultCompany.api.Enums;
+using WilbertVaultCompany.api.Models;
+
+namespace WilbertSvcs.Management.ViewComponents
+{
+    public class EmployeeViewComponent : ViewComponent
+    {
+        private readonly wilbertdbContext _context;
+
+        public EmployeeViewComponent(wilbertdbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            List<Employee> pltEmp = new List<Employee>();
+
+            pltEmp = (from emplist in _context.Employee
+                      where emplist.PlantId == id
+                      select emplist).ToList();
+
+            foreach (var emp in pltEmp)
+            {
+                emp.Title = Enum.GetName(typeof(PlantRole), Int32.Parse(emp.Title));
+                emp.PhoneType1 = Enum.GetName(typeof(PhoneType), Int32.Parse(emp.PhoneType1));
+            }
+
+            return View(pltEmp);
+        }
+    }
+}
--- WilbertVaultCompany.api/WilbertVaultCompany.api/Enums/PlantRole.cs
+++ WilbertVaultCompany.api/WilbertVaultCompany.api/Enums/PlantRole.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace WilbertVaultCompany.api.Enums
 {
     public enum PlantRole
     {
+        [Display(Name = "- Select -")]
         Choose,
+        [Display(Name = "Franchise Owner")]
         FranchiseOwner,
+        [Display(Name = "Franchise Manager")]
         FranchiseManager,
         Driver,
         Sales,
